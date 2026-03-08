7e587be43316f609ebec12ce6929a91d15585eed
wuhao <465989879@qq.com>
2026-03-08 11:07:00 +0800

    Use static splash icon

--- app/src/main/kotlin/com/nongjiqianwen/MainActivity.kt
+++ app/src/main/kotlin/com/nongjiqianwen/MainActivity.kt
@@ -1,12 +1,6 @@
 package com.nongjiqianwen
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
-import android.animation.AnimatorSet
-import android.animation.ObjectAnimator
 import android.os.Bundle
-import android.view.animation.AccelerateDecelerateInterpolator
-import android.view.animation.DecelerateInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
@@ -17,37 +11,8 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        val splashScreen = installSplashScreen()
+        installSplashScreen()
         super.onCreate(savedInstanceState)
-        splashScreen.setOnExitAnimationListener { splashViewProvider ->
-            val iconView = splashViewProvider.iconView
-            iconView.scaleX = 0.9f
-            iconView.scaleY = 0.9f
-
-            val scaleX = ObjectAnimator.ofFloat(iconView, "scaleX", 0.9f, 1f).apply {
-                duration = 720L
-                interpolator = DecelerateInterpolator()
-            }
-            val scaleY = ObjectAnimator.ofFloat(iconView, "scaleY", 0.9f, 1f).apply {
-                duration = 720L
-                interpolator = DecelerateInterpolator()
-            }
-            val fade = ObjectAnimator.ofFloat(splashViewProvider.view, "alpha", 1f, 0f).apply {
-                duration = 120L
-                startDelay = 600L
-                interpolator = DecelerateInterpolator()
-            }
-
-            AnimatorSet().apply {
-                playTogether(scaleX, scaleY, fade)
-                addListener(object : AnimatorListenerAdapter() {
-                    override fun onAnimationEnd(animation: Animator) {
-                        splashViewProvider.remove()
-                    }
-                })
-                start()
-            }
-        }
 
         enableEdgeToEdge(
             statusBarStyle = SystemBarStyle.auto(
