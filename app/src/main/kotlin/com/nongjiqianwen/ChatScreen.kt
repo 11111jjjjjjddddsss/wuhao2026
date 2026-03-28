1d7c84cfa11027b50cdabde0995b7904cf1356d2
wuhao <465989879@qq.com>
2026-03-29 07:04:20 +0800

    Revert "Detach immediately on upward streaming drag"
    This reverts commit 3b2c1e7cde4fd16734473a0a168522a9c8602c2f.

--- app/src/main/kotlin/com/nongjiqianwen/ChatScreen.kt
+++ app/src/main/kotlin/com/nongjiqianwen/ChatScreen.kt
@@ -3093,18 +3093,6 @@ fun ChatScreen() {
                 if (
                     isStreaming &&
                     activeStreamBottomSpacerPx > 0 &&
-                    available.y < 0f &&
-                    !userDetachedFromBottom
-                ) {
-                    pendingResumeAutoFollow = false
-                    userDetachedFromBottom = true
-                    autoScrollMode = AutoScrollMode.Idle
-                    jumpButtonVisible = false
-                    return Offset.Zero
-                }
-                if (
-                    isStreaming &&
-                    activeStreamBottomSpacerPx > 0 &&
                     available.y > 0f &&
                     isAtStreamingFollowLine()
                 ) {
