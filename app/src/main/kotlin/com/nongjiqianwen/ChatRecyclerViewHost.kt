c1889d73d3f082ccb3462197de308b1275f3e825
wuhao <465989879@qq.com>
2026-04-12 22:47:17 +0800

    Keep send anchor request alive until item exists

--- app/src/main/kotlin/com/nongjiqianwen/ChatRecyclerViewHost.kt
+++ app/src/main/kotlin/com/nongjiqianwen/ChatRecyclerViewHost.kt
@@ -178,10 +178,6 @@ internal fun ChatRecyclerViewHost(
                 if (pendingStartAnchorRequestId <= 0 || pendingStartAnchorPosition < 0) {
                     activeStartAnchorRequestId.intValue = 0
                     setStartAnchorLayoutSuppressed(false)
-                    if (pendingStartAnchorRequestId > 0 && pendingStartAnchorPosition < 0) {
-                        lastAppliedStartAnchorRequestId.intValue = pendingStartAnchorRequestId
-                        onPendingStartAnchorHandled()
-                    }
                 }
                 val shouldApplyPendingStartAnchor =
                     pendingStartAnchorRequestId > 0 &&
