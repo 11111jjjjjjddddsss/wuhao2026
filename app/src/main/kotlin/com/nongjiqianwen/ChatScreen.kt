91ed0d923dee21bb713b38f766b30377c266d1f5
wuhao <465989879@qq.com>
2026-04-26 19:58:38 +0800

    Revert "Defer streaming block follow until stable bottom"
    This reverts commit 656b8bffc1ddb62d081384b3337c1e4c4f620d4f.

--- app/src/main/kotlin/com/nongjiqianwen/ChatScreen.kt
+++ app/src/main/kotlin/com/nongjiqianwen/ChatScreen.kt
@@ -3773,20 +3773,6 @@ fun ChatScreen() {
         ) {
             return@LaunchedEffect
         }
-        withFrameNanos { }
-        if (
-            !isStreaming ||
-            !hasStreamingItem ||
-            scrollMode != ScrollMode.AutoFollow ||
-            scrollRuntime.userInteracting.value ||
-            chatListUserDragging ||
-            sendStartAnchorActive ||
-            !startupLayoutReady ||
-            chatListState.firstVisibleItemIndex != 0 ||
-            chatListState.firstVisibleItemScrollOffset != 0
-        ) {
-            return@LaunchedEffect
-        }
         chatListState.requestScrollToItem(index = 0)
     }
     restoreBottomAnchorIfNeededAfterStreamingStop =
