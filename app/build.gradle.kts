e787a74335418a72f573e736844f9cf59ff48564
wuhao <465989879@qq.com>
2026-06-23 17:44:19 +0800

    Bump Android release to 1.0.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         applicationId = "com.nongjiqiancha"
         minSdk = 24
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.0.8"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         buildConfigField("String", "UPLOAD_BASE_URL", "\"$uploadBaseUrl\"")
