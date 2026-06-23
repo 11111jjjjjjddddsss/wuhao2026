efc3a8bbb87befa30988dbdbcc886a5be3d0b9b2
wuhao <465989879@qq.com>
2026-06-23 13:28:30 +0800

    Record stabilized divider test package

--- scripts/publish-android-test-apk.ps1
+++ scripts/publish-android-test-apk.ps1
@@ -135,6 +135,7 @@ function Test-PublicDownloadUrl {
     )
     try {
         if ($Method -eq "GetRange") {
+            Add-Type -AssemblyName System.Net.Http -ErrorAction Stop
             $handler = [System.Net.Http.HttpClientHandler]::new()
             $handler.AllowAutoRedirect = $true
             $client = [System.Net.Http.HttpClient]::new($handler)
